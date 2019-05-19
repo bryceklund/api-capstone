158f1daad91da2f1d201be66b39d266534f0460a
Bryce Eklund <bryce.eklund@gmail.com>
2019-05-19 14:44:41 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+function callYelp(lat, long, rating) {
+    const url = `https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${long}&radius=3200&categories=divebars&price=1`;
+    const header = {
+        method: 'get',
+        headers: {
+            'Authorization': 'Bearer ajlYJ0-yupZgYj18Vjfi_fGGbPRNFzxzb0d0j3WvORtVRIp6--oYrjRf-ZDdwT1XiVvKB_gHNZ0AcUr9G-U0_ZY4AOX--7ivRRDDYyxUAJEqDiHs2e1z3HFVS4XfXHYx'
+        }
+    };
+    fetch(url, header)
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson));
+}
+
+function getData(position) {
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+    const rating = $("input[name='star']:checked").val();
+    callYelp(lat, long, rating);
+}
+
+function handleGo() {
+    $('.go').click(event => {
+        console.log('go button clicked');
+        event.preventDefault();
+        navigator.geolocation.getCurrentPosition(getData);
+    })
+}
+
+$(handleGo);
