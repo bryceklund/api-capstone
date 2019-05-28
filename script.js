35db6c6f378e07a3e8c1d79a3711a14255cee868
Bryce Eklund <bryce.eklund@gmail.com>
2019-05-28 16:40:09 -0700

    completed feature implementation; began styling

--- script.js
+++ script.js
@@ -1,81 +1,106 @@
-function sortBy(arg, sel, elem) { //taken from the internet
-    console.log('sortBy is running');
-        let element = $(sel).children(elem);
-        if (arg.includes("name")) {
-            console.log('name sort is running');
-            if (arg.includes("desc")) {
-                element.sort();
-            } else {
-                element.sort();
-                element.reverse();
-            }
-            $(element).detach().appendTo($(sel));
-            console.log(element);
-        } else {
-            console.log('int sort is running');
-            element.sort(function(a, b) {
-                let an = parseInt(a.getAttribute(arg)),
-                bn = parseInt(b.getAttribute(arg));
-                if (arg.includes("asc")) {
-                        if (an > bn)
-                        return 1;
-                        if (an < bn)
-                        return -1;
-                } else if (arg.includes("desc")) {
-                        if (an < bn)
-                        return 1;
-                        if (an > bn)
-                        return -1;
+function sortBy(arr, key, dir) {
+    if (key == 'name') {
+        if (dir == 'asc') {
+            return arr.sort(function(a, b) {
+                let nameA  = a['name'].toLowerCase(), nameB = b['name'].toLowerCase();
+                if (nameA < nameB) { //sort string ascending
+                    return -1;
                 }
-                return 0;
-            });
-            console.log($(element));
-
-            console.log('sorting complete - attempting to manipulate DOM');
-            $(element).detach().appendTo($(sel));
+                if (nameA > nameB) {
+                    return 1;   
+                }
+                return 0; //default return value (no sorting)
+            }); 
+        } else if (dir == 'desc') {
+            return arr.sort(function(a, b) {
+                let nameA  = a['name'].toLowerCase(), nameB = b['name'].toLowerCase();
+                if (nameA < nameB) { //sort string descending
+                    return 1;
+                }
+                if (nameA > nameB) {
+                    return -1;   
+                }
+                return 0; //default return value (no sorting)
+        });
         }
+    } else {
+        if (dir == 'asc') {
+            return arr.sort((a, b) => {
+                return b[key] - a[key];
+            });
+        } else if (dir == 'desc') {
+            return arr.sort((a, b) => {
+                return a[key] - b[key];
+            });
+        }
+    }
 }
 
-
-function handleSort() {
+function handleSort(results) {
     console.log('handleSort running...');
     $('#sort-option').change(function(event) {
         const option = $('#sort-option option:selected').val();
-        console.log(option);
-        sortBy(option, '.results', 'li');
+        let sorted = sortBy(results, option.split('-', 2)[0], option.split('-', 2)[1]);
+        if (option != "") {
+            $('.results').empty();
+            for (let i = 0; i < sorted.length; i++) {
+                $('.results').append(`<li class="result" name="${sorted[i]["name"]}" distance="${(sorted[i]["distance"] / 1609.344).toFixed(2)}" rating="${sorted[i]["rating"]}" busyvalue="">
+                <a href="${sorted[i]["url"]}" target="_blank">
+                    <div class="">
+                    <img src="${sorted[i]["image_url"]}" alt="business photo">
+                    <h2>${sorted[i]["name"]}</h2>
+                    <p>Distance: ${(sorted[i]["distance"] / 1609.344).toFixed(2)} miles<br>
+                        <span class="address">Address: ${sorted[i]["location"]["address1"]}</span><br>
+                        <span class="rating">rating: ${sorted[i]["rating"]}</span>
+                    </p>
+                    </div>
+                </a>
+                </li>`
+            )};
+        }
+
     });
 }
 
 function showData(json, rating) {
     $('.results').empty();
-    $('.home').addClass('hidden');
+    $('.loading').addClass('hidden');
     $('.results-container').removeClass('hidden');
     $('.try-again').click(event => appReset());
-    handleSort();
-
+    let results = [];
+    console.log(json.businesses);
     for (let i = 0; i < json.total; i++) {
         if (json['businesses'][i].rating <= rating && json['businesses'][i]["is_closed"] == false) {
+            results.push(json['businesses'][i]);
             console.log(json['businesses'][i]);
-            $('.results').append(`<li name="${json['businesses'][i]["name"]}" distance="${(json['businesses'][i]["distance"] / 1609.344).toFixed(2)}" rating="${json['businesses'][i]["rating"]}" busyvalue="">
+            $('.results').append(`<li class="result" name="${json['businesses'][i]["name"]}" distance="${(json['businesses'][i]["distance"] / 1609.344).toFixed(2)}" rating="${json['businesses'][i]["rating"]}" busyvalue="">
             <a href="${json['businesses'][i]["url"]}" target="_blank">
-                <div class="result">
+                <div class="">
                 <img src="${json['businesses'][i]["image_url"]}" alt="business photo">
                 <h2>${json['businesses'][i]["name"]}</h2>
                 <p>Distance: ${(json['businesses'][i]["distance"] / 1609.344).toFixed(2)} miles<br>
                     <span class="address">Address: ${json['businesses'][i]["location"]["address1"]}</span><br>
-                    <span class="hours">closed now? ${json['businesses'][i]["is_closed"]}</span><br>
                     <span class="rating">rating: ${json['businesses'][i]["rating"]}</span>
                 </p>
                 </div>
             </a>
             </li>`);
         }
-    }
 
+    }
+    if (results.length == 0) {
+        $('.results').append(`<p class="no-results">No results found!</p>`);
+    }
+    handleSort(results);
+    console.log(results);
 }
 
-function callYelp(lat, long, rating) {
-    const url = "https://cors-anywhere.herokuapp.com" + `/api.yelp.com:443/v3/businesses/search?latitude=${lat}&longitude=${long}&radius=3200&categories=divebars&price=1`;
+function callYelp(lat, long, rating, couches) {
+    let url = "https://cors-anywhere.herokuapp.com" + `/api.yelp.com:443/v3/businesses/search?latitude=${lat}&longitude=${long}&radius=3200&categories=divebars&price=1`;
+    if (couches) {
+        console.log('couches!');
+        url = "https://cors-anywhere.herokuapp.com" + `/api.yelp.com:443/v3/businesses/search?term=couch&latitude=${lat}&longitude=${long}&radius=3200&categories=divebars&price=1`;
+    }
     const header = {
         method: 'get',
         headers: {
@@ -87,22 +112,37 @@ function callYelp(lat, long, rating) {
     console.log(url);
     const ratingval = rating;
     fetch(url, header)
-    .then(response => response.json())
-    .then(responseJson => showData(responseJson, ratingval));
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(responseJson => showData(responseJson, ratingval))
+    .catch(err => {
+        $('.error-text').text(`Something went wrong: ${err.message}`.toUpperCase())
+    });
 }
 
 function getData(position) {
+    $('.home').addClass('hidden');
+    $('.loading').removeClass('hidden');
     const lat = position.coords.latitude;
     const long = position.coords.longitude;
     const rating = $(".stars input:checked").val();
+    const couches = $('#couches:checked').val();
+    console.log(couches);
     console.log(lat, long);
     console.log(rating);
-    callYelp(lat, long, rating);
+    callYelp(lat, long, rating, couches);
 }
 
 function handleGo() {
     $('.stars input').click(event => {
-
+        let label = $(`label[for=${$(this).attr('id')}]`);
+        console.log(label);
+        $(`label[for=${$(event.currentTarget).attr('id')}]`).css('color', '#ffc700');
+        $(`label[for!=${$(event.currentTarget).attr('id')}]`).css('color', '');
         console.log($('.stars input:checked').val())
     });
     $('#go').click(event => {
@@ -110,8 +150,8 @@ function handleGo() {
         console.log('go button clicked');
         event.preventDefault();
         navigator.geolocation.getCurrentPosition(getData);
-        
-    })
+
+    });
 }
 
 function appReset() {
